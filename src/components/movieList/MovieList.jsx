10ae562e597064d35527e9cafb04f32e58bade58
Tochka <tochka114@gmail.com>
2024-09-12 23:34:12 +0300

    list

---
+++ src/components/movieList/MovieList.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Loader from "../loader/Loader"; 
+
+
+const MovieList = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  const url = 'https://api.themoviedb.org/3/trending/movie/day';
+
+  const options = {
+    headers: {
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YTE3ZTYwMWIzMWQwYzNmOTQ1Y2UxMzI4YTYzNTUyOCIsIm5iZiI6MTcyNjA0Mzc2Ni42MzMxODgsInN1YiI6IjY2ZTE0ZjI3NDVlYzhhN2VkN2FmZTc1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dxlPzrbGefKwDvO72FHRFQUtAU-D2YVwtqqkA7HvKM8'
+    }
+  };
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get(url, options);
+        setTrendingMovies(response.data.results); 
+      } catch (error) {
+        console.error('Error fetching trending movies:', error);
+      } finally {
+        setLoading(false); 
+      }
+    };
+
+    fetchMovies();
+  }, []); 
+
+  
+  if (loading) {
+    return <Loader />;
+  }
+
+  
+  return (
+    <div>
+      {trendingMovies.length > 0 ? (
+        <ul>
+          {trendingMovies.map((movie) => (
+            <li key={movie.id}>{movie.title}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No movies found</p>
+      )}
+    </div>
+  )
+      }
+
+      export default MovieList
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,42 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+
+import MovieList from '../../components/movieList/MovieList';
 
 const HomePage = () => {
-  const [trendingMovies, setTrendingMovies] = useState(null);
+  
+  
 
-  const url = 'https://api.themoviedb.org/3/trending/movie/day';
-
-  const options = {
-    headers: {
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YTE3ZTYwMWIzMWQwYzNmOTQ1Y2UxMzI4YTYzNTUyOCIsIm5iZiI6MTcyNjA0Mzc2Ni42MzMxODgsInN1YiI6IjY2ZTE0ZjI3NDVlYzhhN2VkN2FmZTc1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dxlPzrbGefKwDvO72FHRFQUtAU-D2YVwtqqkA7HvKM8'
-    }
-  };
-
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await axios.get(url, options);
-        setTrendingMovies(response.data.results);
-      } catch (error) {
-        console.error('Error fetching trending movies:', error);
-      }
-    };
-
-    fetchMovies();
-  }, []); 
-
+  
   return (
     <div>
-      <h1>Trending Movies</h1>
-      {trendingMovies ? (
-        <ul>
-          {trendingMovies.map((movie) => (
-            <li key={movie.id}>{movie.title}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <h1>Trending today</h1>
+      <MovieList/>
     </div>
   );
 };
