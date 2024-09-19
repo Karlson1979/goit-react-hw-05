f057220ea9584d56d084b3e9f8ce986b1f358f4e
Tochka <tochka114@gmail.com>
2024-09-19 12:12:32 +0300

    no commentary

--- src/pages/moviesPage/MoviesPage.jsx
+++ src/pages/moviesPage/MoviesPage.jsx
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const MoviesPage = () => {
-    const [search, setSearch] = useState(''); // Строка поиска
-    const [movies, setMovies] = useState([]); // Массив с результатами поиска фильмов
-    const [error, setError] = useState(''); // Ошибка, если что-то пойдет не так
+    const [search, setSearch] = useState(''); 
+    const [movies, setMovies] = useState([]); 
+    const [error, setError] = useState(''); 
 
-    const API_KEY = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YTE3ZTYwMWIzMWQwYzNmOTQ1Y2UxMzI4YTYzNTUyOCIsIm5iZiI6MTcyNjA0Mzc2Ni42MzMxODgsInN1YiI6IjY2ZTE0ZjI3NDVlYzhhN2VkN2FmZTc1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dxlPzrbGefKwDvO72FHRFQUtAU-D2YVwtqqkA7HvKM8'; // Ваш API ключ
+    const API_KEY = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YTE3ZTYwMWIzMWQwYzNmOTQ1Y2UxMzI4YTYzNTUyOCIsIm5iZiI6MTcyNjA0Mzc2Ni42MzMxODgsInN1YiI6IjY2ZTE0ZjI3NDVlYzhhN2VkN2FmZTc1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dxlPzrbGefKwDvO72FHRFQUtAU-D2YVwtqqkA7HvKM8'; 
 
-    // Обработчик изменения строки поиска
+    
     const handleChange = (event) => {
         setSearch(event.target.value);
     };
 
-    // Обработчик отправки формы
+    
     const handleSubmit = async (event) => {
         event.preventDefault();
         if (search.trim() === '') {
@@ -21,7 +21,7 @@ const MoviesPage = () => {
             return;
         }
 
-        setError(''); // Сбрасываем ошибку перед новым запросом
+        setError(''); 
 
         try {
             const response = await axios.get(
@@ -29,18 +29,18 @@ const MoviesPage = () => {
                 {
                     params: {
                         query: search,
-                        include_adult: false, // Исключаем взрослые фильмы
-                        language: 'en-US', // Устанавливаем язык
-                        page: 1, // Номер страницы
+                        include_adult: false, 
+                        language: 'en-US', 
+                        page: 1, 
                     },
                     headers: {
-                        Authorization: API_KEY, // Передача API-ключа через заголовок
+                        Authorization: API_KEY, 
                     },
                 }
             );
 
             if (response.data.results && response.data.results.length > 0) {
-                setMovies(response.data.results); // Обновляем список фильмов
+                setMovies(response.data.results); // 
             } else {
                 setError('No results found.');
             }
